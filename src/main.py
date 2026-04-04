1c483c3915f5588e8304064f1c777119d46b7ed0
Andrew Lin <greenpenguin2005@outlook.com>
2026-04-04 19:03:26 -0400

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import cv2
+import mss
+import numpy as np
+
+def main():
+    print("Welcome to the Map Scanner!")
+    # We will add our logic here once you have installed Tesseract
+    # and saved your template images.
+
+if __name__ == "__main__":
+    main()
